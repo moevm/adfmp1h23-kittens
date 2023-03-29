a2f379f682eb7e1dce40b406dbad7c38c65a866e
boggoriss <boriskust01@gmail.com>
2023-03-29 12:57:59 +0300

    app is done

--- app/src/main/java/com/example/kittens_catalog/features/chats/KittenChatsFragment.kt
+++ app/src/main/java/com/example/kittens_catalog/features/chats/KittenChatsFragment.kt
@@ -1,12 +1,16 @@
 package com.example.kittens_catalog.features.chats
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.text.InputType
 import android.view.View
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.kittens_catalog.R
 import com.example.kittens_catalog.databinding.FragmentChatsBinding
 import com.example.kittens_catalog.features.base.BaseFragment
+
 
 class KittenChatsFragment : BaseFragment<FragmentChatsBinding>(R.layout.fragment_chats) {
     override val binding: FragmentChatsBinding by viewBinding()
@@ -29,6 +33,20 @@ class KittenChatsFragment : BaseFragment<FragmentChatsBinding>(R.layout.fragment
             chatAdapter.setData(
                 MockChat.list
             )
+            addButton.setOnClickListener {
+                val input = EditText(requireContext())
+                input.inputType = InputType.TYPE_CLASS_TEXT
+                val builder = AlertDialog.Builder(context)
+                builder
+                    .setMessage("Enter name whom you want to text")
+                    .setCancelable(true)
+                    .setView(input)
+                    .setPositiveButton("Add") { a, b ->
+                        MockChat.list.add(Message(name = input.text.toString(), value = "hello!"))
+                        chatAdapter.setData(MockChat.list)
+                    }
+                builder.create().show()
+            }
         }
     }
 }
